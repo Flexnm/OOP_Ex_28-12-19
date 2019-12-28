f32bbbdc4e7fa1e401ebaf844a25330f9a9b2e9b
Flexnm <58367828+Flexnm@users.noreply.github.com>
2019-12-28 15:28:15 +0200

    Update PassText.java

--- winwixEx/PassText.java
+++ winwixEx/PassText.java
@@ -8,7 +8,7 @@ public class PassText extends TextBox {
 
 	public String draw() {
 		String str = "";
-		for (int j = 0; j < 3; j++) {
+		for (int j = 0; j < 3; j++) { // is j < 3 because it needs to be 3 lines of text.
 			for (int i = 0; i < getLabel().length(); i++) {
 				str += "*";
 			}
