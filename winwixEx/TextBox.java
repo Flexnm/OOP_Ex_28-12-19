eb9fb23390b1df82092b962641917827eb0d9201
Flexnm <58367828+Flexnm@users.noreply.github.com>
2019-12-28 15:31:43 +0200

    Update TextBox.java

--- winwixEx/TextBox.java
+++ winwixEx/TextBox.java
@@ -27,15 +27,15 @@ public class TextBox extends Control {
 	public String draw() {
 		String str = "";
 		if (getCharNum() > 0 && getCharNum() >= getLabel().length()) {
-			str += stars(getCharNum(), getLabel());	
+			str += stars(getCharNum(), getLabel());	// Helper method used here.
 		} else {
-			str += str += stars(8, "");
+			str += str += stars(8, ""); // Helper method used here too.
 		}
 		return str;
 	}
 
 	public static String stars(int num, String text) { // I decided to make a helper method.
-		String str = "";
+		String str = ""; // I use the helper method because I didnt want to type it twice.
 		for (int i = 0; i < num; i++) {
 			str +=  "*"; // It builds the * lines above and below the label text
 		}
